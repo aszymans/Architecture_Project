a864116eb2afe82ac9dc610ade66c274ef67593b
Alan Malta Rodrigues <alan.malta@cern.ch>
2023-04-26 15:13:49 -0400

    Add logging to the script; remove progressbar

--- VGG19.py
+++ VGG19.py
@@ -1,3 +1,5 @@
+import logging
+import sys
 import tensorflow as tf
 from tensorflow.keras.applications import VGG19
 from tensorflow.keras.models import Model
@@ -6,6 +8,29 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow_datasets as tfds
 
+# FIXME TODO: update number of epochs here
+# FYI: each epoch takes around 8k seconds in a CRC single GPU
+EPOCHS = 2
+
+
+def setup_logger():
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(module)s:%(message)s")
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+    return root
+
+
+logger = setup_logger()
+logger.info("Starting training of VGG19 with imagenette dataset.")
+
+# print GPU devices configuration
+logger.info("GPU configuration: %s", tf.python.client.device_lib.list_local_devices())
+
+tfds.disable_progress_bar()
 (ds_train, ds_val), ds_info = tfds.load(
     'imagenette',
     split=['train', 'validation'],
@@ -37,7 +62,11 @@ for layer in base_model.layers:
 
 model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-history = model.fit(ds_train, epochs=10, validation_data=ds_val)
+# verbose 1=progress bar; 2=one line per epoch
+history = model.fit(ds_train, epochs=EPOCHS, verbose=2, validation_data=ds_val,
+                    workers=4, use_multiprocessing=True)
+
+logger.info(f"Finished training with output metrics: {model.get_metrics_result()}")
 
 for layer in model.layers[:11]: # You can choose a different layer number for fine-tuning
     layer.trainable = False
@@ -47,4 +76,9 @@ for layer in model.layers[11:]:
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Fine-tune the model
-history_fine_tune = model.fit(ds_train, epochs=10, validation_data=ds_val)
+history_fine_tune = model.fit(ds_train, epochs=EPOCHS, verbose=2, validation_data=ds_val,
+                              workers=4, use_multiprocessing=True)
+
+logger.info("Finished training of VGG19 with imagenette dataset.")
+
+# Saving and load this model: https://www.tensorflow.org/api_docs/python/tf/keras/Model#example_12
